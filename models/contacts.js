3f67375d04981fd0719abe445117403dc5abaa70
Oleg <116948332+oliverplay@users.noreply.github.com>
2023-07-30 08:53:07 +0300

    v2

--- models/contacts.js
+++ models/contacts.js
@@ -1,66 +1,74 @@
-const fs = require("fs").promises;
-const path = require("path");
-const contactPath = path.join(__dirname, "contacts.json");
-const { v4 } = require("uuid");
+const fs = require("fs").promises; // Import the fs module to handle file operations
+const path = require("path"); // Import the path module to work with file paths
+const contactPath = path.join(__dirname, "contacts.json"); // Define the path to the contacts.json file
+const { v4 } = require("uuid"); // Import the uuid module to generate unique IDs
 
+// Function to list all contacts
 const listContacts = async () => {
-  let data = await fs.readFile(contactPath);
-  data = JSON.parse(data);
-  return data;
+  let data = await fs.readFile(contactPath); // Read the contacts.json file
+  data = JSON.parse(data); // Parse the JSON data into a JavaScript object
+  return data; // Return the parsed data (list of contacts)
 };
 
+// Function to get a contact by its ID
 const getContactById = async (contactId) => {
-  let data = await fs.readFile(contactPath);
-  data = JSON.parse(data);
-  const singleContact = data.filter((item) => item.id === contactId);
-  console.log(singleContact);
+  let data = await fs.readFile(contactPath); // Read the contacts.json file
+  data = JSON.parse(data); // Parse the JSON data into a JavaScript object
+  const singleContact = data.filter((item) => item.id === contactId); // Find the contact with the given ID
+  console.log(singleContact); // Output the found contact to the console (for debugging purposes)
   if (singleContact.length > 0) {
-    return [singleContact, 200];
+    return [singleContact, 200]; // Return the found contact and the status code 200 (OK)
   } else {
-    return [{ message: "Not found" }, 404];
+    return [{ message: "Not found" }, 404]; // Return a message and the status code 404 (Not Found)
   }
 };
 
+// Function to remove a contact by its ID
 const removeContact = async (contactId) => {
-  let data = await fs.readFile(contactPath);
-  data = JSON.parse(data);
+  let data = await fs.readFile(contactPath); // Read the contacts.json file
+  data = JSON.parse(data); // Parse the JSON data into a JavaScript object
 
+  // Filter out the contact with the given ID and create a new array (newData) without it
   const newData = data.filter((item) => item.id !== contactId);
 
-  await fs.writeFile(contactPath, JSON.stringify(newData));
+  await fs.writeFile(contactPath, JSON.stringify(newData)); // Write the new data back to the contacts.json file
 
-  return [{ message: "User was deleted" }, 200];
+  return [{ message: "User was deleted" }, 200]; // Return a message and the status code 200 (OK)
 };
 
+// Function to add a new contact
 const addContact = async (body) => {
   if (!body.name || !body.phone || !body.email) {
-    return Promise.resolve([{ message: "missing required name field" }, 400]);
+    return Promise.resolve([{ message: "missing required name field" }, 400]); // If required fields are missing, return an error with the status code 400 (Bad Request)
   }
   const newContact = {
     name: body.name, // Changed from body.username
     email: body.email,
     phone: body.phone,
-    id: v4(),
+    id: v4(), // Generate a new unique ID using uuid
   };
-  let data = await fs.readFile(contactPath);
-  data = JSON.parse(data);
-  data.push(newContact);
-  console.log([newContact, 200]);
+  let data = await fs.readFile(contactPath); // Read the contacts.json file
+  data = JSON.parse(data); // Parse the JSON data into a JavaScript object
+  data.push(newContact); // Add the new contact to the existing data array
+  console.log([newContact, 200]); // Output the newly added contact and the status code 200 (OK) to the console (for debugging purposes)
+
+  // Write the updated data back to the contacts.json file and return the new contact along with the status code 200 (OK)
   return fs.writeFile(contactPath, JSON.stringify(data)).then(() => {
     return [newContact, 200];
   });
 };
 
+// Function to update an existing contact by its ID
 const updateContact = async (contactId, body) => {
-  let data = await fs.readFile(contactPath);
-  data = JSON.parse(data);
-  const userIndex = data.findIndex((item) => item.id === contactId);
+  let data = await fs.readFile(contactPath); // Read the contacts.json file
+  data = JSON.parse(data); // Parse the JSON data into a JavaScript object
+  const userIndex = data.findIndex((item) => item.id === contactId); // Find the index of the contact with the given ID in the array
   if (userIndex === -1) {
-    return [{ error: "ID doesn't exist" }, 404];
+    return [{ error: "ID doesn't exist" }, 404]; // If contact not found, return an error with the status code 404 (Not Found)
   }
-  data[userIndex] = { ...data[userIndex], ...body };
-  await fs.writeFile(contactPath, JSON.stringify(data));
-  return [{ message: "User was updated" }, 200];
+  data[userIndex] = { ...data[userIndex], ...body }; // Merge the existing contact with the updated properties from the request body
+  await fs.writeFile(contactPath, JSON.stringify(data)); // Write the updated data back to the contacts.json file
+  return [{ message: "User was updated" }, 200]; // Return a message and the status code 200 (OK)
 };
 
 module.exports = {
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,56 +1,61 @@
-const express = require("express");
+const express = require("express"); // Import the Express framework
 const {
   listContacts,
   getContactById,
   addContact,
   removeContact,
   updateContact,
-} = require("../../models/contacts");
-const router = express.Router();
+} = require("../../models/contacts"); // Import the contact handling functions from the contacts module
+const router = express.Router(); // Create an instance of Express Router
 
+// Route: GET / - List all contacts
 router.get("/", async (req, res, next) => {
   try {
-    const data = await listContacts();
-    res.json(data);
+    const data = await listContacts(); // Call the listContacts function to retrieve all contacts
+    res.json(data); // Respond with the list of contacts in JSON format
   } catch (err) {
-    next(err);
+    next(err); // Pass any encountered errors to the error handling middleware
   }
 });
 
+// Route: GET /:contactId - Get a specific contact by ID
 router.get("/:contactId", async (req, res, next) => {
   try {
-    const [data, status] = await getContactById(req.params.contactId);
-    res.status(status).json(data);
+    const [data, status] = await getContactById(req.params.contactId); // Call the getContactById function to retrieve a contact by its ID
+    res.status(status).json(data); // Respond with the contact data along with the corresponding status code (e.g., 200 for success, 404 for not found)
   } catch (err) {
-    next(err);
+    next(err); // Pass any encountered errors to the error handling middleware
   }
 });
 
+// Route: POST / - Add a new contact
 router.post("/", async (req, res, next) => {
   try {
-    const [data, status] = await addContact(req.body);
-    res.status(status).json(data);
+    const [data, status] = await addContact(req.body); // Call the addContact function to add a new contact with the provided data in the request body
+    res.status(status).json(data); // Respond with the new contact data along with the corresponding status code (e.g., 200 for success, 400 for bad request)
   } catch (err) {
-    next(err);
+    next(err); // Pass any encountered errors to the error handling middleware
   }
 });
 
+// Route: DELETE /:contactId - Remove a contact by ID
 router.delete("/:contactId", async (req, res, next) => {
   try {
-    const [data, status] = await removeContact(req.params.contactId, req.body);
-    res.status(status).json(data);
+    const [data, status] = await removeContact(req.params.contactId, req.body); // Call the removeContact function to delete a contact by its ID
+    res.status(status).json(data); // Respond with a message indicating the success of the deletion along with the corresponding status code (e.g., 200 for success, 404 for not found)
   } catch (err) {
-    next(err);
+    next(err); // Pass any encountered errors to the error handling middleware
   }
 });
 
+// Route: PUT /:contactId - Update an existing contact by ID
 router.put("/:contactId", async (req, res, next) => {
   try {
-    const [data, status] = await updateContact(req.params.contactId, req.body);
-    res.status(status).json(data);
+    const [data, status] = await updateContact(req.params.contactId, req.body); // Call the updateContact function to update a contact by its ID with the provided data in the request body
+    res.status(status).json(data); // Respond with a message indicating the success of the update along with the corresponding status code (e.g., 200 for success, 404 for not found)
   } catch (err) {
-    next(err);
+    next(err); // Pass any encountered errors to the error handling middleware
   }
 });
 
-module.exports = router;
+module.exports = router; // Export the router to be used in the main app file
